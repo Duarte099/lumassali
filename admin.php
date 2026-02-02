e3f79d7b29ffacd21001f4896097aef17cfc7f9b
Duarte099 <a14395@oficina.pt>
2026-02-02 01:38:37 +0000

    correção de erro

--- admin.php
+++ admin.php
@@ -24,7 +24,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="../css/style.css">
-    <title>OneMarket | Admin</title>
+    <title>Lumassali | Admin</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
